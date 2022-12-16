4786938cc4786c1e6ed40b6cae9f518697cf9c19
nirdeshkumar02 <saini.nirdesh.kumar@gmail.com>
2022-12-16 13:28:46 +0530

    update script.groovy

--- script.groovy
+++ script.groovy
@@ -5,10 +5,10 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
+    withCredentials([usernamePassword(credentialsId: 'docker-creds', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t nirdeshkumar02/jenkins-java-app:jma-2 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
+        sh 'docker push nirdeshkumar02/jenkins-java-app:jma-2'
     }
 } 
 
